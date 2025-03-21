3f35e014df1b0950f189e73e073884b6e371e53d
Denis Kabakov <denvams@gmail.com>
2025-03-21 11:50:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/musicharmony/utils/MusicSelector.kt
@@ -0,0 +1,5 @@
+package com.example.musicharmony.utils
+
+class MusicSelector {
+    // Пустой класс, реализуем позже
+}
---
+++ app/src/main/java/com/example/musicharmony/utils/NoiseAnalyzer.kt
@@ -0,0 +1,5 @@
+package com.example.musicharmony.utils
+
+class NoiseAnalyzer {
+    // Пустой класс, реализуем позже
+}
